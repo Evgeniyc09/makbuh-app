5c71e605c844d84798fc591ad71efee9f90e8554
Evgeniy <evgeniyc09@gmail.com>
2025-05-27 19:14:31 +0500

    feat: main page

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", (event) => {
+	document.querySelectorAll('.tab-buttons button').forEach(button => {
+		button.addEventListener('click', () => {
+			const tabNumber = button.getAttribute('data-tab');
 
+			// Удалить класс active у всех кнопок и контента
+			document.querySelectorAll('.tab-buttons button').forEach(btn => btn.classList.remove('active'));
+			document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+
+			// Добавить класс active к выбранной кнопке и соответствующему контенту
+			button.classList.add('active');
+			document.querySelector(`.tab-content[data-content="${tabNumber}"]`).classList.add('active');
+		});
+	});
 });
