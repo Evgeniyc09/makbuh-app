95b10592d78d1e183a40017c3cdc2a346b2405f4
Evgeniy <evgeniyc09@gmail.com>
2025-07-01 22:58:26 +0500

    feat: compass

--- app/js/app.js
+++ app/js/app.js
@@ -13,4 +13,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			document.querySelector(`.tab-content[data-content="${tabNumber}"]`).classList.add('active');
 		});
 	});
+
+	initModal()
 });
+
+function initModal() {
+	let lastModalOpener = null;
+
+	function closeAllModals() {
+		document.querySelectorAll('.modal.open').forEach(modal => {
+			modal.classList.remove('open');
+		});
+	}
+	document.addEventListener('click', function (e) {
+		let modalOpen = e.target.closest('[data-modal-open]');
+		if (modalOpen) {
+			e.preventDefault();
+			const id = modalOpen.getAttribute('data-modal-open');
+			const modal = document.getElementById(id);
+			if (modal) modal.classList.add('open');
+			lastModalOpener = modalOpen;
+			return;
+		}
+		if (e.target.hasAttribute('data-modal-close')) {
+			e.preventDefault();
+			const modal = e.target.closest('.modal');
+			if (modal) modal.classList.remove('open');
+		}
+	});
+
+	window.closeAllModals = closeAllModals;
+}
